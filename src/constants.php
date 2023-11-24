d8c4700173d7c2da804f1a216423606bd5df00ff
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-11-24 17:03:57 +0200

    Make constant definition conditional

--- src/constants.php
+++ src/constants.php
@@ -5,10 +5,13 @@
  * @license    AGPL-3.0-or-later
  */
 
-/**
- * The minimum Akeeba Backup JSON API level supported by this library
- *
- * @var   int
- * @since 1.0.0
- */
-const AKEEBA_JSON_BACKUP_API_MINIMUM_API_LEVEL = 319;
+if (!defined('AKEEBA_JSON_BACKUP_API_MINIMUM_API_LEVEL'))
+{
+	/**
+	 * The minimum Akeeba Backup JSON API level supported by this library
+	 *
+	 * @var   int
+	 * @since 1.0.0
+	 */
+	define('AKEEBA_JSON_BACKUP_API_MINIMUM_API_LEVEL', 319);
+}
